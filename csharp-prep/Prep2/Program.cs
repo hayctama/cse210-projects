1b95f5862eafacef3934eab46b8547002f4f34d3
hayctama <hay23019@byui.edu>
2024-04-24 19:59:42 -0600

    prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade? ");
+        string stringGrade = Console.ReadLine();
+        int numGrade = int.Parse(stringGrade);
+
+        string letter = "";
+
+
+        if (numGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (numGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (numGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (numGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (numGrade >= 70)
+        {
+            Console.WriteLine("You passed the course. Congrats! ");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass. Good luck next time! ");
+        }
+
     }
 }
