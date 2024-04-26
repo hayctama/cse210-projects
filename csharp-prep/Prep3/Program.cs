14bf7319a5d5f24fdef9daee44833f808bcd3b9b
hayctama <hay23019@byui.edu>
2024-04-26 10:50:36 -0600

    prep 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Used part 1 and 2.
+        // Console.Write("What is the magic number? ");
+        // int magicNum = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 11);
+
+        int guess = -100;
+
+        while (guess != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            string letter = "";
+
+            if (magicNum > guess)
+            {
+                letter = "higher";
+            }
+            else if (magicNum < guess)
+            {
+                letter = "lower";
+            }
+            else
+            {
+                letter = "You guessed it!";
+            }
+
+            Console.WriteLine(letter);
+        }
+
+        
     }
 }
