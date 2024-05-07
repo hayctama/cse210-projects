d8321ee341dc0a182128cbdf3aef79276232a9c3
hayctama <hay23019@byui.edu>
2024-05-07 08:42:27 -0600

    develop03 starting

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -9,16 +9,30 @@ public class Reference
 
     public Reference(string book, int chapter, int verse)
     {
-
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
     }
 
     public Reference(string book, int chapter, int startVerse, int endVerse)
     {
-
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
     }
 
     public string GetDisplayText()
     {
-        return "";
+        if (_endVerse == null)
+        {
+            string referenceDisplay = $"{_book} {_chapter} : {_verse}";
+        }
+        else
+        {
+            string referenceDisplay = $"{_book} {_chapter} : {_verse} {_endVerse}";
+        }
+
+        return referenceDisplay;
     }
 }
